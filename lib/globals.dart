4cfbb9f375649f8fda10fd6dd2b5945749074d86
andrewfady <1700307@eng.asu.edu.eg>
2022-04-30 14:56:10 +0200

    Global variables adding

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,8 +1,14 @@
 library project.globals;
 
+import 'package:flutter/material.dart';
+
 import 'dart:ui' as ui;
 
 var parsedata;
 var temp_id;
-const domain = "http://2fb5-217-55-251-137.ngrok.io";
-ui.Image? image_data;
+const domain = "https://1680-102-40-118-194.ngrok.io";
+var image_data;
+var width;
+var height;
+bool st = false;
+int cnt = 0;
